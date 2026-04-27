d0dba13933d8e4f6f16ff90c151f8687e6019c5d
tarekmostafa <egypty3@gmail.com>
2026-04-27 07:44:20 +0300

    test: update widget test to use BrutalBMICalculator widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:brutalist_bmi_calculator/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const BrutalBMICalculator());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
